f6fafb3d3ec79f8d0f3587da52e425f9493be47d
Shanshan Liu <shanshanliu@Shanshans-MacBook-Pro.local>
2022-03-02 09:47:32 -0800

    passed all tests

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
+class RetiredForagerBee  extends Bee{
+  constructor (age,job,canFly,color,food,treasureChest){
+    super(food,treasureChest);
+    this.age = 40;
+    this.job = "gamble";
+    this.canFly = false;
+    this.color = "grey";
+    this.treasureChest = [];
+  }
+  forage(){
+    return "I am too old, let me play cards instead"
+  }
+  gamble(treasure){
+    this.treasureChest.push(treasure);
+  }
   // TODO..
 };
